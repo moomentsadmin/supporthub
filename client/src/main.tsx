8acf4848402ad8efb80dded28a0268298c17ef9a
Bapi Raju <mooments_admin@volyty.com>
2026-02-14 20:06:25 +0530

    deplyment-issue.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,4 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./components/ui/button-fix.css";
-
 createRoot(document.getElementById("root")!).render(<App />);
